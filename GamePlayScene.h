51f8de5bcfd01783e66f18ce45417ea1134050fb
Tetsuya4430 <k020g1184@g.neec.ac.jp>
2022-04-20 00:36:23 +0900

    プロジェクト ファイルを追加します。

---
+++ GamePlayScene.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "BaseScene.h"
+#include "Sprite.h"
+#include "Object3d.h"
+#include "Input.h"
+#include "ObjectManager.h"
+
+#include <memory>
+
+/// <summary>
+/// ゲームプレイシーン
+/// </summary>
+class GamePlayScene : public BaseScene
+{
+public:
+	//初期化
+	void Initialize() override;
+
+	//終了処理
+	void Finalize() override;
+
+	//更新
+	void Update() override;
+
+	//描画
+	void Draw() override;
+
+	
+private:
+	Sprite* sprite = nullptr;
+	Model* model_1 = nullptr;
+	Model* model_2 = nullptr;
+	Input* input = nullptr;
+
+	std::shared_ptr<Object3d> object3d_1;
+	std::shared_ptr<Object3d> object3d_2;
+	std::shared_ptr<Object3d> object3d_3;
+	std::unique_ptr<ObjectManager> objectManager_1;
+	std::unique_ptr<ObjectManager> objectManager_2;
+	std::unique_ptr<ObjectManager> objectManager_3;
+
+	float VY = 0.0f;	//Y方向の速度
+	const float gravity = -9.8f / 60.0f;	//重力
+	float y = 60.0f;	//高さの初期値
+
+
+};
+
